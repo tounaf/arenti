0d8cbe02d49faf90998412b153251722d8901673
fetra <harinjatovo.fetra@gmail.com>
2019-06-04 13:41:53 +0300

    new ressource article

---
+++ src/Arenti/BlogBundle/ArentiBlogBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Arenti\BlogBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class ArentiBlogBundle extends Bundle
+{
+}
---
+++ src/Arenti/BlogBundle/Entity/Article.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace Arenti\BlogBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Article
+ *
+ * @ORM\Table(name="article")
+ * @ORM\Entity(repositoryClass="Arenti\BlogBundle\Repository\ArticleRepository")
+ */
+class Article
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="libele", type="string", length=255, nullable=true)
+     */
+    private $libele;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="Content", type="string", length=255, nullable=true)
+     */
+    private $content;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set libele
+     *
+     * @param string $libele
+     *
+     * @return Article
+     */
+    public function setLibele($libele)
+    {
+        $this->libele = $libele;
+
+        return $this;
+    }
+
+    /**
+     * Get libele
+     *
+     * @return string
+     */
+    public function getLibele()
+    {
+        return $this->libele;
+    }
+
+    /**
+     * Set content
+     *
+     * @param string $content
+     *
+     * @return Article
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    /**
+     * Get content
+     *
+     * @return string
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+}
